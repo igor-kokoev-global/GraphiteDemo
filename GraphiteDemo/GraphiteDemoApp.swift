94093003c6f8697cc3e13d4be34719b6e0b2cec9
Igor Kokoev <igor.kokoev@global.com>
2024-07-05 14:03:50 +0100

    Initial Commit

---
+++ GraphiteDemo/GraphiteDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphiteDemoApp.swift
+//  GraphiteDemo
+//
+//  Created by Igor Kokoev on 05/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct GraphiteDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
